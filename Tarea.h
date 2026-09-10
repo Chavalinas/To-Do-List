fd17f2c28c05dc37d1097fa45464ea5db20ac679
Camila <camila.andreamtz@gmail.com>
2026-09-10 09:20:26 -0600

    Commit inicial de los archivos header y cpp de la clase Tarea

---
+++ Tarea.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Tarea
+{
+    private:
+        string descripcion;
+        string estado;
+        string fecha;
+
+    public:
+        Tarea(string descripcion, string estado, string fecha_creacion);
+
+        void obtener_informacion();
+        string obtener_descripcion();
+        string obtener_estado();
+        string obtener_fecha_creacion();
+
+        void modificar_descripcion(string desc);
+        void modificar_estado(string status);
+        void modificar_fecha(string nueva_fecha);
+};
